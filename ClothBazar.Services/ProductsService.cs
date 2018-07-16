6a774db6d50c7d2990ed5cb00dede449e6b7b95c
Sajjad Gul <sajjadarifgul@gmail.com>
2018-07-16 20:34:24 +0500

    AJAX

---
+++ ClothBazar.Services/ProductsService.cs
@@ -0,0 +1,59 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+    public class ProductsService
+    {
+
+        public Product GetProduct(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+
+        public List<Product> GetProducts()
+        {
+            using (var context = new CBContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public void SaveProduct(Product product)
+        {
+            using (var context = new CBContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                var product = context.Products.Find(ID);
+
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ClothBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductTable(string search)
+        {
+            var products = productsService.GetProducts();
+
+            if(string.IsNullOrEmpty(search) == false)
+            {
+                products = products.Where(p => p.Name != null && p.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+            
+            return PartialView(products);
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+
+            return RedirectToAction("ProductTable");
+        }
+
+    }
+}
